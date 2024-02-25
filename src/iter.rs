391e000ff1bf064146686f8d5c2d8014cc3038c5
Nordgaren <nordgarentv@gmail.com>
2024-02-25 13:37:17 -0700

    update crate name

--- src/iter.rs
+++ src/iter.rs
@@ -43,7 +43,7 @@ impl<'a> Iterator for Iter<'a> {
         }
 
         // Get a reference to the chunk header and then advance the buffer to the start of the chunk
-        let chunk = self.buffer.read_t::<ChunkHeader>().unwrap();
+        let chunk = self.buffer.read_t::<ChunkHeader>().ok()?;
 
         // Keep track of the current section so that we can check the next time we call `next()`
         self.current_section = chunk.get_chunk_type();
@@ -51,10 +51,10 @@ impl<'a> Iterator for Iter<'a> {
         // Get the length of the chunk data from the header, get a slice containing the chunk data,
         // and then advance the buffer to the start of the crc data.
         let chunk_data_len = chunk.get_length() as usize;
-        let chunk_data = self.buffer.read_bytes(chunk_data_len).unwrap();
+        let chunk_data = self.buffer.read_bytes(chunk_data_len).ok()?;
 
         // Get a reference to the crc value and then advance the buffer to the start of the next chunk.
-        let crc = self.buffer.read_t::<ChunkCRC>().unwrap();
+        let crc = self.buffer.read_t::<ChunkCRC>().ok()?;
 
         Some(ChunkRefs::new(chunk, chunk_data, crc))
     }
